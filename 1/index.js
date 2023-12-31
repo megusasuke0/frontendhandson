bef869554a7711906f6791eab06274a04b4e76e3
megusasuke0 <116707505+megusasuke0@users.noreply.github.com>
2023-12-31 22:47:31 +0900

    add li to ul

--- 1/index.js
+++ 1/index.js
@@ -1,1 +1,11 @@
-console.log('1!');
+//lesson01--------------------------------------------------
+//chatGPT prompt："javascript DOM作成 li テキスト「これです」"
+//reference:https://www.wakuwakubank.com/posts/306-javascript-dom/#index_id4
+//reference:https://stackoverflow.com/questions/50250214/how-to-use-insertbefore-without-second-parameter
+
+//create list item
+const newList = document.getElementById('newlist'); // get:ID='newlist'
+console.log(newList);
+const listItem = document.createElement('li'); // create<li></li>
+const newContent =  listItem.textContent = 'これです' // add:text -> これです
+newList.insertBefore(listItem , null); 
