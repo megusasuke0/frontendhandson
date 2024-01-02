f51788dc3d010fa89f085a44c1eb0566d8f8b176
megusasuke0 <116707505+megusasuke0@users.noreply.github.com>
2024-01-02 12:06:48 +0900

    add:webpack_vite

--- 1/index.js
+++ 1/index.js
@@ -4,7 +4,7 @@
 //reference:https://stackoverflow.com/questions/50250214/how-to-use-insertbefore-without-second-parameter
 
 //create list item
-const jsList = document.getElementById('js-list'); // get:ID='js-list'
+const List = document.getElementById('js-list'); // get:ID='js-list'
 const listItem = document.createElement('li'); // create<li></li>
 listItem.textContent = 'これです' // add:text -> これです
-jsList.appendChild(listItem);
+List.appendChild(listItem);
