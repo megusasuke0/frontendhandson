3b3cce187683ab003ede191bd1308ad94592fb63
megusasuke0 <116707505+megusasuke0@users.noreply.github.com>
2024-01-02 22:53:17 +0900

    feat:ul to li add link and img

--- 2/index.js
+++ 2/index.js
@@ -1,10 +1,18 @@
-//lesson01--------------------------------------------------
-//chatGPT prompt："javascript DOM作成 li テキスト「これです」"
-//reference:https://www.wakuwakubank.com/posts/306-javascript-dom/#index_id4
-//reference:https://stackoverflow.com/questions/50250214/how-to-use-insertbefore-without-second-parameter
+//lesson02--------------------------------------------------
+//chatGPT prompt："javascript DOM作成 li aタグにテキスト挿入「これです」 aタグの中にimgタグ"
+//reference:https://developer.mozilla.org/ja/docs/Learn/JavaScript/Client-side_web_APIs/Manipulating_documents
 
 //create list item
 const List = document.getElementById('js-list'); // get:ID='js-list'
 const listItem = document.createElement('li'); // create<li></li>
-listItem.textContent = 'これです' // add:text -> これです
+const listItemLink = document.createElement('a'); //create<a></a>
+const listItemLinkImg = document.createElement('img'); //create<img>
+
+listItemLink.href = "1.html";//add:linkURL
+listItemLink.textContent = 'これです' // add:text -> これです
+listItemLinkImg.src = "image/bookmark.png";//add:img -> src
+listItemLinkImg.alt = "ブックマーク";//add:img -> alt
+
+listItemLink.appendChild(listItemLinkImg);
+listItem.appendChild(listItemLink);
 List.appendChild(listItem);
