ee51e4ca9a649d8739e37e38b458e0a5f00d4a94
megusasuke0 <116707505+megusasuke0@users.noreply.github.com>
2024-01-08 14:26:32 +0900

    fix:var-name,function

--- 4/index.js
+++ 4/index.js
@@ -2,14 +2,14 @@
 //chatGPT prompt："javascript DOM作成 li aタグにテキスト挿入「これです」 aタグの中にimgタグ 配列 複数 for文でループ"
 //reference:https://qiita.com/yrn03m/items/87e223acc6e733b50429    (参考）連想配列
 
-//create list item
-const createList = document.getElementById('js-list');
+//list item
+const items = document.getElementById('js-items');
 const arrayItems = [
   {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
   {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}];
 const fragmentItem = document.createDocumentFragment();
 
-arrayItems.forEach(function (arrayItem) {
+arrayItems.forEach(arrayItem => {
   const listItem = document.createElement('li');
   const listItemLink = document.createElement('a');
   const listItemLinkImg = document.createElement('img');
@@ -23,4 +23,4 @@ arrayItems.forEach(function (arrayItem) {
   fragmentItem.appendChild(listItem);
 });
 
-  createList.appendChild(fragmentItem);
+  items.appendChild(fragmentItem);
